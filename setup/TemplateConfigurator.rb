85936b6768be5bb7726a1e3dbccddd203b6632a9
iPanda <wang_w_t@qq.com>
2019-04-03 20:59:27 +0800

    更新模版用于创建工程

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -113,7 +113,7 @@ module Pod
     end
 
     def replace_variables_in_files
-      file_names = ['POD_LICENSE', 'POD_README.md', '.travis.yml', podfile_path]
+      file_names = ['POD_LICENSE', 'POD_README.md', podfile_path]
       file_names.each do |file_name|
         text = File.read(file_name)
         text.gsub!("${POD_NAME}", @pod_name)
