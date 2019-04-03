a7fce762c59f1565e187d68a17adc8608519bb6c
iPanda <wang_w_t@qq.com>
2019-04-03 11:31:40 +0800

    Update MessageBank.rb

--- setup/MessageBank.rb
+++ setup/MessageBank.rb
@@ -36,7 +36,7 @@ module Pod
 
       puts "\n------------------------------"
       puts ""
-      puts "To get you started we need to ask a few questions, this should only take a minute."
+      puts "from longlongago====To get you started we need to ask a few questions, this should only take a minute."
       puts ""
 
       has_run_before = `defaults read org.cocoapods.pod-template HasRunbefore`.chomp == "1"
