c7dc293fdeba53ce675bf0b415e3691824ed099b
longlongago <wang_w_t@qq.com>
2019-04-04 09:37:57 +0800

    使用sdwebimage加载网络图片

--- templates/ios/PROJECT/PROJECT/CPDViewController.m
+++ templates/ios/PROJECT/PROJECT/CPDViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CPDViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface CPDViewController ()
 
@@ -17,12 +18,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 300, 100, 100)];
-    label.text = @"这是一个测试页面";
+    // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(30, 100, 100, 100)];
+    label.text = @"这里使用SDWebImage加载网络图片";
     [label sizeToFit];
     [self.view addSubview:label];
+    
+    UIImageView *view = [[UIImageView alloc] initWithFrame:CGRectMake(10, 150, 100, 100)];
+    NSURL* url = [[NSURL alloc] initWithString:@"https://pic1.zhimg.com/v2-3b4fc7e3a1195a081d0259246c38debc_1200x500.jpg"];
+    [view sd_setImageWithURL:url];
+    [self.view addSubview:view];
 }
+
 
 - (void)didReceiveMemoryWarning
 {
